cb657e1a8294feeee43e7f0858c02ebafa612580
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-12-05 19:12:41 +0100

    Actualizado el script

--- genera_trabajo.sh
+++ genera_trabajo.sh
@@ -4,6 +4,7 @@ rm Odoo.md
 touch Odoo.md
 echo -e "# Odoo" > Odoo.md
 cat ./Trabajo/Informacion-ERP.md >> Odoo.md
+cat ./Trabajo/funcionalidades.md >> Odoo.md
 cat ./Trabajo/empresas.md >> Odoo.md
 clear
 echo -e "Trabajo generado."
